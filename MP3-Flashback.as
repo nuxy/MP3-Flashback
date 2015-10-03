539d1c487b30de5d86e29eb7f13b4907983183a9
Marc S. Brooks <marc@mbrooks.info>
2015-10-03 11:14:56 -0700

    Replaced tabs with 2 spaces for formatting in HTML, CSS and JavaScript files
    Updated copyright year and website address

--- MP3-Flashback.as
+++ MP3-Flashback.as
@@ -1,8 +1,8 @@
-/*
+/**
  *  MP3 Flashbock
- *  An elegant MP3 Flash fallback when HTML5 audio is not supported
+ *  An elegant MP3 Flash fallback when HTML5 audio is not supported.
  *
- *  Copyright 2012-2013, Marc S. Brooks (http://mbrooks.info)
+ *  Copyright 2012-2015, Marc S. Brooks (https://mbrooks.info)
  *  Licensed under the MIT license:
  *  http://www.opensource.org/licenses/mit-license.php
  *
@@ -16,13 +16,13 @@ import flash.media.Sound;
 import flash.media.SoundChannel;
 import flash.media.SoundTransform;
 
-// enable external callbacks
+// Enable external callbacks
 ExternalInterface.addCallback('player', soundPlayer);
 
-// import flashVars
+// Import flashVars
 var param:Object = LoaderInfo(this.root.loaderInfo).parameters;
 
-// register globals
+// Register globals
 var isLoading:Boolean = false;
 var isPlaying:Boolean = false;
 var startTime:Number  = 0.0;
@@ -30,7 +30,7 @@ var soundFile:Sound;
 var soundChan:SoundChannel;
 var soundTran:SoundTransform;
 
-/*
+/**
  * Initialize sound object
  */
 (function init() {
@@ -38,15 +38,15 @@ var soundTran:SoundTransform;
 	soundChan = new SoundChannel();
 	soundTran = new SoundTransform();
 
-	// .. bind events
+	// .. Bind events
 	soundFile.load(new URLRequest(param.file0));
 	soundFile.addEventListener(Event.COMPLETE, loadComplete);
 	soundFile.addEventListener(ProgressEvent.PROGRESS, loadProgress);
 	soundFile.addEventListener(IOErrorEvent.IO_ERROR, onIOError);
 })();
 
-/*
- * Callbacks when loading files; return progress as a percent
+/**
+ * Callbacks when loading files; return progress as a percent.
  */
 function loadProgress(e:Event) {
 	if (soundFile && soundFile.length > 0) {
@@ -65,8 +65,8 @@ function loadComplete(e:Event) {
 	}
 }
 
-/*
- * Callbacks when playing MP3s; returns duration and percent as function arguments
+/**
+ * Callbacks when playing MP3s; returns duration and percent as function arguments.
  */
 function playProgress(e:Event) {
 	if (!isLoading) {
@@ -91,8 +91,8 @@ function playComplete() {
 	ExternalInterface.call('$.fn.playComplete');
 }
 
-/*
- * MP3 player controls: Play, Pause, Stop, and Volume events
+/**
+ * MP3 player controls: Play, Pause, Stop, and Volume events.
  */
 function soundPlayer(action, volValue=null):void {
 	if (isLoading) { return }
@@ -133,8 +133,8 @@ function soundPlayer(action, volValue=null):void {
 	}
 }
 
-/*
- * Other functions
+/**
+ * Handle errors.
  */
 function onIOError(Event:IOErrorEvent) {
 	trace('Failed to load MP3 file: ' + Event.text);
--- MP3-Flashback.js
+++ MP3-Flashback.js
@@ -1,8 +1,8 @@
-/*
+/**
  *  MP3 Flashback
  *  An elegant MP3 Flash fallback when HTML5 audio is not supported
  *
- *  Copyright 2012-2013, Marc S. Brooks (http://mbrooks.info)
+ *  Copyright 2012-2015, Marc S. Brooks (https://mbrooks.info)
  *  Licensed under the MIT license:
  *  http://www.opensource.org/licenses/mit-license.php
  *
@@ -13,239 +13,239 @@
  */
 
 (function($) {
-	var soundPlayer, buttonPause, buttonPlay, buttonStop, progressBar, duraTimer, volumeBar;
+  var soundPlayer, buttonPause, buttonPlay, buttonStop, progressBar, duraTimer, volumeBar;
 
-	var methods = {
-		"init" : function(options) {
+  var methods = {
+    "init" : function(options) {
 
-			// default option
-			var settings = $.extend({
-				volumeStart : 70,
-				tracks      : null
-			}, options);
+      // Default options.
+      var settings = $.extend({
+        volumeStart : 70,
+        tracks      : null
+      }, options);
 
-			return this.each(function() {
-				var $this = $(this),
-					data  = $this.data();
+      return this.each(function() {
+        var $this = $(this),
+          data  = $this.data();
 
-				if ( $.isEmptyObject(data) ) {
-					soundPlayer = $this;
-					buttonPause = $this.children('.button_pause');
-					buttonPlay  = $this.children('.button_play');
-					buttonStop  = $this.children('.button_stop');
-					progressBar = $this.children('.progress_bar');
-					duraTimer   = $this.children('.duration');
-					volumeBar   = $this.children('.volume_bar');
+        if ( $.isEmptyObject(data) ) {
+          soundPlayer = $this;
+          buttonPause = $this.children('.button_pause');
+          buttonPlay  = $this.children('.button_play');
+          buttonStop  = $this.children('.button_stop');
+          progressBar = $this.children('.progress_bar');
+          duraTimer   = $this.children('.duration');
+          volumeBar   = $this.children('.volume_bar');
 
-					var soundFile = settings.tracks.file0;
-					var audioObj  = null;
+          var soundFile = settings.tracks.file0;
+          var audioObj  = null;
 
-					try {
-						$.fn.loadProgress();
+          try {
+            $.fn.loadProgress();
 
-						audioObj = new Audio(soundFile);
-					}
-					catch(err) {}
+            audioObj = new Audio(soundFile);
+          }
+          catch(err) {}
 
-					// check support for HTML5 audio/MPEG3
-					if (audioObj && audioObj.canPlayType('audio/mp3') ) {
-						$.fn.loadComplete();
+          // Check support for HTML5 audio/MPEG3
+          if (audioObj && audioObj.canPlayType('audio/mp3') ) {
+            $.fn.loadComplete();
 
-						$this.data({
-							soundObj : audioObj,
-							options  : settings
-						});
+            $this.data({
+              soundObj : audioObj,
+              options  : settings
+            });
 
-						$this.MP3Flashback('setup');
-					}
+            $this.MP3Flashback('setup');
+          }
 
-					// .. fallback to Flash
-					else {
-						var objectId = genRandStr(10);
-						var flashObj = $('<object></object>').attr('id', objectId);
+          // .. fallback to Flash
+          else {
+            var objectId = genRandStr(10);
+            var flashObj = $('<object></object>').attr('id', objectId);
 
-						$this.append(flashObj);
+            $this.append(flashObj);
 
-						swfobject.embedSWF('MP3-Flashback.swf', objectId, '0','0','9.0','', settings.tracks, 'transparent','',
-							function() {
-								$this.data({
-									soundObj : window.document[objectId],
-									useFlash : true,
-									options  : settings
-								});
+            swfobject.embedSWF('MP3-Flashback.swf', objectId, '0','0','9.0','', settings.tracks, 'transparent','',
+              function() {
+                $this.data({
+                  soundObj : window.document[objectId],
+                  useFlash : true,
+                  options  : settings
+                });
 
-								$this.MP3Flashback('setup');
-							}
-						);
-					}
-				}
-			});
-		},
+                $this.MP3Flashback('setup');
+              }
+            );
+          }
+        }
+      });
+    },
 
-		"destroy" : function() {
-			return this.each(function() {
-				$(this).removeData();
-			});
-		},
+    "destroy" : function() {
+      return this.each(function() {
+        $(this).removeData();
+      });
+    },
 
-		"setup" : function() {
-			return this.each(function() {
-				var $this = $(this),
-					data = $this.data();
+    "setup" : function() {
+      return this.each(function() {
+        var $this = $(this),
+          data = $this.data();
 
-				buttonStop.attr('disabled','true');
+        buttonStop.attr('disabled','true');
 
-				// initalize volume slider
-				volumeBar.slider({
-					range : 'min',
-					min   : 1,
-					max   : 100,
-					value : data.options.volumeStart,
-					slide : function(e, ui) {
-						var soundVol = parseFloat(ui.value / 100);
+        // Initalize volume slider
+        volumeBar.slider({
+          range : 'min',
+          min   : 1,
+          max   : 100,
+          value : data.options.volumeStart,
+          slide : function(e, ui) {
+            var soundVol = parseFloat(ui.value / 100);
 
-						if (data.useFlash) {
-							data.soundObj.player('volume', soundVol);
-						}
-						else {
-							data.soundObj.volume = soundVol;
-						}
-					}
-				});
+            if (data.useFlash) {
+              data.soundObj.player('volume', soundVol);
+            }
+            else {
+              data.soundObj.volume = soundVol;
+            }
+          }
+        });
 
-				// enable mouse events; toggle play/pause button visibility
-				buttonPause.click(function() {
-					if (data.useFlash) {
-						data.soundObj.player('pause');
-					}
-					else {
-						data.soundObj.pause();
-					}
+        // Enable mouse events; toggle play/pause button visibility.
+        buttonPause.click(function() {
+          if (data.useFlash) {
+            data.soundObj.player('pause');
+          }
+          else {
+            data.soundObj.pause();
+          }
 
-					buttonPauseEvent();
-				});
+          buttonPauseEvent();
+        });
 
-				buttonPlay.click(function() {
-					if (data.useFlash) {
-						data.soundObj.player('play');
-					}
-					else {
-						data.soundObj.play();
-					}
+        buttonPlay.click(function() {
+          if (data.useFlash) {
+            data.soundObj.player('play');
+          }
+          else {
+            data.soundObj.play();
+          }
 
-					buttonPlayEvent();
-				});
+          buttonPlayEvent();
+        });
 
-				buttonStop.click(function() {
-					if (data.useFlash) {
-						data.soundObj.player('stop');
-					}
-					else {
-						data.soundObj.pause();
-						data.soundObj.currentTime = 0;
-					}
+        buttonStop.click(function() {
+          if (data.useFlash) {
+            data.soundObj.player('stop');
+          }
+          else {
+            data.soundObj.pause();
+            data.soundObj.currentTime = 0;
+          }
 
-					buttonStopEvent();
-				});
+          buttonStopEvent();
+        });
 
-				// enable HTML5 audio events
-				$(data.soundObj).bind('timeupdate', function() {
-					var audioObj = data.soundObj;
-					var minutes  = Math.floor(audioObj.currentTime / 60) % 60;
-					var seconds  = Math.floor(audioObj.currentTime % 60);
-					var duration = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-					var percent  = Math.ceil(audioObj.currentTime / audioObj.duration * 100);
+        // Enable HTML5 audio events.
+        $(data.soundObj).bind('timeupdate', function() {
+          var audioObj = data.soundObj;
+          var minutes  = Math.floor(audioObj.currentTime / 60) % 60;
+          var seconds  = Math.floor(audioObj.currentTime % 60);
+          var duration = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+          var percent  = Math.ceil(audioObj.currentTime / audioObj.duration * 100);
 
-					if (percent == 100) {
-						buttonStop.trigger('click');
-					}
-					else {
-						$.fn.playProgress(duration, percent);
-					}
-				});
+          if (percent == 100) {
+            buttonStop.trigger('click');
+          }
+          else {
+            $.fn.playProgress(duration, percent);
+          }
+        });
 
-				$(data.soundObj).bind('canplay', function() {
-					$.fn.loadComplete();
-				});
-			});
-		}
-	};
+        $(data.soundObj).bind('canplay', function() {
+          $.fn.loadComplete();
+        });
+      });
+    }
+  };
 
-	$.fn.MP3Flashback = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		}
-		else
-		if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		}
-		else {
-			$.error('Method ' +  method + ' does not exist on jQuery.MP3Flashback');
-		}
-	};
+  $.fn.MP3Flashback = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else
+    if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    }
+    else {
+      $.error('Method ' +  method + ' does not exist on jQuery.MP3Flashback');
+    }
+  };
 
-	/*
-	 * HTML5 audio/Flash callback functions
-	 */
-	$.fn.loadComplete = function() {
-		soundPlayer.removeClass('loading');
-		soundPlayer.children('.button_play, .button_stop, div, span').fadeIn('slow');
-	};
+  /**
+   * HTML5 audio/Flash callback functions.
+   */
+  $.fn.loadComplete = function() {
+    soundPlayer.removeClass('loading');
+    soundPlayer.children('.button_play, .button_stop, div, span').fadeIn('slow');
+  };
 
-	$.fn.loadProgress = function(percent) {
-		soundPlayer.addClass('loading');
-		soundPlayer.children('button, div, span').css({ display : 'none' });
-	};
+  $.fn.loadProgress = function(percent) {
+    soundPlayer.addClass('loading');
+    soundPlayer.children('button, div, span').css({ display : 'none' });
+  };
 
-	$.fn.playComplete = function() {
-		progressBar.progressbar({ value : 0 });
-		duraTimer.html('0:00');
-		buttonStopEvent();
-	};
+  $.fn.playComplete = function() {
+    progressBar.progressbar({ value : 0 });
+    duraTimer.html('0:00');
+    buttonStopEvent();
+  };
 
-	$.fn.playProgress = function(duration, percent) {
-		progressBar.progressbar({ value : percent });
-		duraTimer.html(duration);
-	};
+  $.fn.playProgress = function(duration, percent) {
+    progressBar.progressbar({ value : percent });
+    duraTimer.html(duration);
+  };
 
-	/*
-	 * Shared event functions
-	 */
-	function buttonPauseEvent() {
-		buttonPause.hide(0);
-		buttonPlay.show(0);
-		buttonStop.removeAttr('disabled');
-		progressBar.children('.ui-progressbar-value')
-			.switchClass('play','stop');
-	}
+  /**
+   * Shared event functions
+   */
+  function buttonPauseEvent() {
+    buttonPause.hide(0);
+    buttonPlay.show(0);
+    buttonStop.removeAttr('disabled');
+    progressBar.children('.ui-progressbar-value')
+      .switchClass('play','stop');
+  }
 
-	function buttonPlayEvent() {
-		buttonPlay.hide(0);
-		buttonPause.show(0);
-		buttonStop.removeAttr('disabled');
-		progressBar.children('.ui-progressbar-value')
-			.switchClass('stop','play');
-	}
+  function buttonPlayEvent() {
+    buttonPlay.hide(0);
+    buttonPause.show(0);
+    buttonStop.removeAttr('disabled');
+    progressBar.children('.ui-progressbar-value')
+      .switchClass('stop','play');
+  }
 
-	function buttonStopEvent() {
-		buttonPause.hide(0);
-		buttonPlay.show(0);
-		buttonStop.attr('disabled','true');
-		progressBar.children('.ui-progressbar-value')
-			.removeClass('stop');
-	}
+  function buttonStopEvent() {
+    buttonPause.hide(0);
+    buttonPlay.show(0);
+    buttonStop.attr('disabled','true');
+    progressBar.children('.ui-progressbar-value')
+      .removeClass('stop');
+  }
 
-	/*
-	 *  Generate a pseudo-random string
-	 */
-	function genRandStr(len) {
-		var count = (len) ? len : 8;
-		var chars = 'abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ0123456789'.split('');
-		var str   = '';
+  /**
+   * Generate a pseudo-random string.
+   */
+  function genRandStr(len) {
+    var count = (len) ? len : 8;
+    var chars = 'abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ0123456789'.split('');
+    var str   = '';
 
-		for (var i = 0; i < count; i++) {
-			str += chars[ Math.floor(Math.random() * chars.length) ];
-		}
-		return str;
-	}
+    for (var i = 0; i < count; i++) {
+      str += chars[ Math.floor(Math.random() * chars.length) ];
+    }
+    return str;
+  }
 })(jQuery);
